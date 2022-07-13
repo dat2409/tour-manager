cca50717396664ec4a6f1584a20f9d570841add9
azoom-thai-doan-dat <thai.doan.dat@azoom.jp>
2022-07-13 21:08:48 +0700

    Config cicd to deploy api

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -2,6 +2,9 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   ssr: false,
+  publicRuntimeConfig: {
+    TOUR_MANAGER_API_BASE_URL: process.env.TOUR_MANAGER_API_BASE_URL,
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - client',
@@ -84,7 +87,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.TOUR_MANAGER_API_BASE_URL,
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const port = process.env.PORT || 5000;
 const route = require('./routes');
 const multer = require('multer');
 const path = require('path');
@@ -10,6 +9,9 @@ const path = require('path');
 const app = express();
 
 require('dotenv').config()
+const port = process.env.PORT || 8000;
+
+console.log(process.env)
 
 var publicDir = path.join(__dirname,'/public');
 app.use('/public', express.static(publicDir));
